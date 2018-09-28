d477f08717b54efb73c48a2621d1e1c3707a09d7
Saul Torres <sault1000@gmail.com>
2018-09-27 19:43:47 -0500

    Initial Site Code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Home msg="TechPlasma"/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from './components/HelloWorld.vue'
+	import Home from './components/Home.vue'
+	import PersistentMini from './components/PersistentMini.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			HelloWorld,
+			Home,
+			PersistentMini
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin: 0px;
+		width:100%;
+		height: 100%;
+		background-color: black;
+	}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+	<div id="Home">
+		<md-app style="min-height: 100vh;" class="page-container md-layout-column">
+			<!-- <md-app-toolbar class="md-transparent md-dense tbspace" md-elevation="0">
+				<span class="md-title">{{msg}}</span>
+			</md-app-toolbar> -->
+
+			<md-app-drawer :md-active.sync="menuVisible" md-permanent="clipped" md-persistent="mini" class="semi-transparent">
+				<md-list class="transparent">
+					<md-list-item @click="toggleMenu" v-if="!menuVisible">
+						<md-icon>keyboard_arrow_right</md-icon>
+						<span class="md-list-item-text">Open Menu</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu" v-if="menuVisible">
+						<md-icon>keyboard_arrow_left</md-icon>
+						<span class="md-list-item-text">Close Menu</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>home</md-icon>
+						<span class="md-list-item-text">Home</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>face</md-icon>
+						<span class="md-list-item-text">About Me</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>work</md-icon>
+						<span class="md-list-item-text">Work</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>description</md-icon>
+						<span class="md-list-item-text">Resume</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>class</md-icon>
+						<span class="md-list-item-text">Projects</span>
+					</md-list-item>
+					<md-list-item @click="toggleMenu">
+						<md-icon>weekend</md-icon>
+						<span class="md-list-item-text">Home</span>
+					</md-list-item>
+				</md-list>
+
+			</md-app-drawer>
+
+			<md-app-content class="transparent">
+				Hello
+			</md-app-content>
+		</md-app>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		props: {
+			msg: String
+		},
+		data: () => ({
+			menuVisible:false
+		}),
+		methods: {
+			toggleMenu() {
+				this.menuVisible = !this.menuVisible
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.transparent{
+		background-color: transparent !important;
+	}
+	.semi-transparent{
+		background-color: rgba(0,0,0,0.5)!important;
+	}
+	.tbspace{
+		display:flex;
+		justify-content: center;
+	}
+	#Home{
+		text-align: center;
+		vertical-align:bottom;
+	}
+	.md-drawer {
+		width:50%;
+		max-width: 200px;
+	}
+	.md-app{
+		background-image: url('../assets/20170622_150742.jpg');
+		background-position:  50% 70%;
+		background-size: cover;
+	}
+</style>
